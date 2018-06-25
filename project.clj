085ff44a4c7fa0de5aca490183ec28d1f3cb6530
Daniel Estevez <daniel.ef@gmail.com>
2018-06-25 03:06:02 -0500

    Adding Logging & Server

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
 (defproject cognicious/axion "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Lightweight Cross-platform Monitoring Agent"
+  :url "https://github.com/cognicious/axion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.github.oshi/oshi-json "3.6.0"]]
+                 ;; server info
+                 [com.github.oshi/oshi-json "3.6.0"]
+                 ;; webserver
+                 [aleph "0.4.6"]
+                 ;; logging
+                 [org.apache.logging.log4j/log4j-core "2.11.0"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.11.0"]]
   :main cognicious.axion)
--- src/cognicious/axion.clj
+++ src/cognicious/axion.clj
@@ -1,15 +1,32 @@
 (ns cognicious.axion
   (:gen-class)
   (:require
+   [aleph.http :as http]
    [clojure.data.json :as json]
    [clojure.java.io :as io]
-   [clojure.pprint :as pp])
+   [clojure.tools.logging :as log]
+   ;[clojure.pprint :as pp]
+   )
   (:import
    (java.util Properties)
    (oshi.json SystemInfo)
    (oshi.util EdidUtil)))
 
+(def meta-project "META-INF/leiningen/cognicious/axion/project.clj")
 (def properties-filename "oshi.json.properties")
+
+(defn project-clj []
+  (if-let [project (io/resource meta-project)]
+    project
+    (do
+      (log/warn (pr-str {:not-found meta-project :trying-with "project.clj"}))
+      "project.clj")))
+
+(defn name&version
+  "Returns name and version reading project.clj"
+  []
+  (let [[_ name version] (-> (project-clj) slurp read-string vec)]
+    [name version]))
 
 (defn properties-file []
   (with-open [is (-> (io/resource properties-filename)
@@ -22,10 +39,19 @@
     (-> si 
         (.toJSON (properties-file)) 
         .toString
-        (json/read-str :key-fn keyword))))
+        ;(json/read-str :key-fn keyword)
+        )))
+
+(defn handler [{:keys [remote-addr] :as req}]
+  (let [_ (log/info (pr-str {:retrieving :system-info :remote-addr remote-addr}))
+        info (system-info)
+        _ (log/info (pr-str {:publishing :system-info :remote-addr remote-addr}))]
+    {:status 200
+     :headers {"content-type" "application/json"}
+     :body info}))
 
 (defn -main
   [& args]
-  (let [mk1 (System/currentTimeMillis)]
-    (pp/pprint (system-info))
-    (println (- (System/currentTimeMillis) mk1) "ms")))
+  (let [server (http/start-server handler {:port 8080})]
+    (log/info (pr-str {:starting (name&version)}))
+    (.wait-for-close server)))
