a69f767e482cefad9b8e6d5df55fa50cc61e5167
Daniel Estevez <daniel.ef@gmail.com>
2018-06-27 23:45:00 -0500

    Move system logic & add pause/restart feature

--- src/cognicious/axion.clj
+++ src/cognicious/axion.clj
@@ -7,15 +7,10 @@
    [clojure.java.io :as io]
    [clojure.tools.logging :as log]
    [cognicious.screenshot :as screen]
-   ;[clojure.pprint :as pp]
-   )
-  (:import
-   (java.util Properties)
-   (oshi.json SystemInfo)
-   (oshi.util EdidUtil)))
+   [cognicious.system :as sys]))
 
 (def meta-project "META-INF/leiningen/cognicious/axion/project.clj")
-(def properties-filename "oshi.json.properties")
+(def paused (atom false))
 
 (defn project-clj 
   "Returns project.clj into the JAR, otherwise, return local file"
@@ -32,47 +27,60 @@
   (let [[_ name version] (-> (project-clj) slurp read-string vec)]
     [name version]))
 
-(defn properties-file 
-  []
-  (with-open [is (-> (io/resource properties-filename)
-                     (io/input-stream))]
-    (doto (Properties.)
-      (.load is))))
+(defmacro defhandler [name [req-sym tx-sym] & body]
+  `(defn ~name [~req-sym]
+     (let [params# (:query-params ~req-sym)
+           tx# (get params# "tx" (str (java.util.UUID/randomUUID)))
+           _# (log/info (pr-str {:status :start :uri (:uri ~req-sym) :tx tx#}))
+           result# (let [~tx-sym tx#] ~@body)
+           _# (log/debug (pr-str result#))]
+       (log/info (pr-str {:status :end :result-code (:status result#) :uri (:uri ~req-sym) :tx tx#}))
+       result#)))
 
-(defn system-info 
-  []
-  (let [si (SystemInfo.)]
-    (-> si 
-        (.toJSON (properties-file)) 
-        .toString
-        ;(json/read-str :key-fn keyword)
-        )))
+(defhandler info [request tx]
+  (cond-> {:status 204}
+    (not @paused) (assoc :status 200
+                        :headers {"content-type" "application/json"}
+                        :body (sys/info))))
 
-(defn info
-  [{:keys [remote-addr] :as req}]
-  (let [_ (log/info (pr-str {:retrieving :system-info :remote-addr remote-addr}))
-        info (system-info)
-        _ (log/info (pr-str {:publishing :system-info :remote-addr remote-addr}))]
-    {:status 200
-     :headers {"content-type" "application/json"}
-     :body info}))
+(defhandler info-raw [request tx]
+  (cond-> {:status 204}
+    (not @paused) (assoc :status 200
+                        :headers {"content-type" "application/json"}
+                        :body (sys/info-raw))))
 
-(defn index [r]
-  (info r))
+(defhandler index [request tx]
+  (cond-> {:status 204}
+    (not @paused) (assoc :status 200 
+                        :headers {"content-type" "text/plain"} 
+                        :body "hello!")))
 
-(defn screenshot [r]
-  (let [screen (screen/take)]
-    {:status 200
-     :headers {"content-type" "image/png"}
-     :body screen}))
+(defhandler pause [request tx]
+  (reset! paused true)
+  {:status 204})
+
+(defhandler restart [request tx]
+  (reset! paused false)
+  {:status 200
+   :headers {"content-type" "application/json"}
+   :body (json/write-str {:status "restarted"})})
+
+(defhandler screenshot [request tx]
+  (cond-> {:status 204}
+    (not @paused) (assoc :status 200
+                        :headers {"content-type" "image/png"}
+                        :body (screen/take))))
 
 (def handler
   (bidi/make-handler ["/" {"" index
-                           "screenshot" screenshot
-                           "info" info}]))
+                           "info" info
+                           "info-raw" info-raw
+                           "pause" pause
+                           "restart" restart
+                           "screenshot" screenshot}]))
 
 (defn -main
   [& args]
-  (let [server (http/start-server handler {:port 8080})]
+  (let [server (http/start-server handler {:port 5555})]
     (log/info (pr-str {:starting (name&version)}))
     (.wait-for-close server)))
