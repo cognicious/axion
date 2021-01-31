283c537ed35f4dd5e49b3a919bc2fb194a0fdaa1
Daniel Estevez <daniel.ef@gmail.com>
2021-01-31 05:15:31 -0600

    Upgrade clojure

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/cognicious/axion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/java.data "0.1.1"]
                  ;; platform info
                  [com.github.oshi/oshi-core "4.0.0"]
                  ;; communication
-                 [aleph "0.4.6"]
-                 [bidi "2.1.3"]
-                 [clj-http "3.10.0"]
+                 [aleph/aleph "0.4.6"]
+                 [bidi/bidi "2.1.3"]
+                 [clj-http/clj-http "3.10.0"]
                  ;; logging
                  [org.apache.logging.log4j/log4j-core "2.11.0"]
                  [org.apache.logging.log4j/log4j-slf4j-impl "2.11.0"]
